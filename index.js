de3371dda13839124d566c50f3740932c10f8728
Y Ramesh Rao <yrameshrao@gmail.com>
2023-05-22 05:46:36 +0530

    Superficial changes

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-
 import 'path';
 import 'express';
-import {WebSocker} from 'ws';
+import { WebSocker } from 'ws';
 import express from 'express';
 
-const app=express();
+const app = express();
 const HTTP_PORT = process.env.HTTP_PORT || 3000;
 const WS_PORT = process.env.WS_PORT || 3001;
 
-app.get('/client', (request, response)=>{
+app.get('/client', (request, response) => {
     response.sendFile(path.resolve(__dirname, './client.html'));
 });
 
-app.get('/streamer', (request, response)=>{
- response.sendFile(path.resolve(__dirname, './streamer.html'));
+app.get('/streamer', (request, response) => {
+    response.sendFile(path.resolve(__dirname, './streamer.html'));
 });
 
-app.lisent(HTTP_PORT, ()=>{
+app.lisent(HTTP_PORT, () => {
     console.log(`HTTP Sever listening at http://localhost:${HTTP_PORT}`);
 });
 
 // ---
 
-const wsServer = new WebSocket.Server({port: WS_PORT}, () => console.log(`WebSocket Server is listening at ws://localhost:${WS_PORT}`));
+const wsServer = new WebSocket.Server({ port: WS_PORT }, () => console.log(`WebSocket Server is listening at ws://localhost:${WS_PORT}`));
+const connectedClients = [];
+
+wsServer.on('connection', (ws, request) => {
+    console.log('Connected');
+    connectedClients.push(ws);
+
+    ws.on('message', data => {
+        connectedClients.forEach((connectedClient, i) => {
+            if (connectedClient.readState === ws.OPEN) {
+                connectedClient.send(data);
+            }
+
+        });
+    });
